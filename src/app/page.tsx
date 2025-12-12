2d7d938978da3b4b4dd6f616f5342a6ceacc21d9
Anish-2005 <123315153+Anish-2005@users.noreply.github.com>
2025-12-13 03:11:32 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import { SinglePagePortfolio } from "@/app/SinglePagePortfolio";
-import { getNotes, getProjects } from "@/lib/content";
 
 export default function Home() {
-  const projects = getProjects();
-  const notes = getNotes();
-  return <SinglePagePortfolio projects={projects} notes={notes} />;
+  return <SinglePagePortfolio />;
 }
